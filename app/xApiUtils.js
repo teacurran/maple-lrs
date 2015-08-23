f87dabff54ff4660fa1998c2977f4afc4ce8699c
Terrence Curran <tcurran@approachingpi.com>
2015-08-23 15:58:14 -0400

    error in function for getting language map

--- app/xApiUtils.js
+++ app/xApiUtils.js
@@ -1,12 +1,13 @@
 
+
 module.exports.getLanguageMap = function(inContent) {
 	var languageMap = [];
 
 	for (var property in inContent) {
 		if (inContent.hasOwnProperty(property)) {
 			languageMap[languageMap.length] = {
-				language: property.name,
-				value: property
+				language: property,
+				value: inContent[property]
 			}
 		}
 	}
